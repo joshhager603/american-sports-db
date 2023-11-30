8849def1af156c78e897b5c82a6d6def62383c10
Josh Hager <jhager603@gmail.com>
2023-11-30 16:13:13 -0500

    added state machine functionality for menus

--- src/DatabaseFrontend.java
+++ src/DatabaseFrontend.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DatabaseFrontend {
     private static String connectionUrl = "jdbc:sqlserver://cxp-sql-02\\jrh236;"
                 + "database=AmericanSports;"
@@ -7,8 +9,80 @@ public class DatabaseFrontend {
                 + "trustServerCertificate=true;"
                 + "loginTimeout=15;";
 
-    // Connect to database.
+    public static void mainMenu(){
+        System.out.print("\nWelcome to the American Sports Database. What would you like to query?\n"+
+                         "0. Exit Program\n" +
+                         "1. Cities\n" +
+                         "\n" +
+                         "Choice: ");
+    }
+
+    public static void citiesMenu(){
+        System.out.print("\nWhat would you like to do?\n"+
+                         "0. Back\n" +
+                         "1. Add a city\n" +
+                         "2. Delete a city\n" +
+                         "3. Update the population of a city\n" +
+                         "\n" +
+                         "Choice: ");
+    }
+
+    public static void insertCityMenu(Scanner s){
+        System.out.print("\nName: ");
+        String name = s.nextLine();
+
+        System.out.print("\nState: ");
+        String state = s.nextLine();
+
+        System.out.print("\nPopulation: ");
+        int population = s.nextInt();
+
+        Cities.insertCity(name, state, population, connectionUrl);
+
+        System.out.print("\nSuccessfully added " + name + "!");
+    }
+
+    public static void deleteCityMenu(Scanner s){
+        System.out.print("\nCity: ");
+        String name = s.nextLine();
+
+        System.out.println(name);
+
+        Cities.deleteCity(name, connectionUrl);
+        System.out.print("\nSuccessfully deleted " + name + ".");
+    }
+
+    public static void updateCityPopMenu(Scanner s){
+        System.out.print("\nCity: ");
+        String name = s.nextLine();
+
+        System.out.print("\nNew population: ");
+        int newPop = s.nextInt();
+
+        System.out.println(name);
+        System.out.println(newPop);
+
+        Cities.updateCityPop(name, newPop, connectionUrl);
+        System.out.print("\nSuccessfully updated " + name + "!");
+    }
+
     public static void main(String[] args) {
-        Cities.updateCityPop("Detroit", 10, connectionUrl);
+        Scanner s = new Scanner(System.in);
+
+        StateMachine stateMachine = new StateMachine(s);
+
+        mainMenu();
+
+       while(true){
+            String input = s.nextLine();
+
+            stateMachine.feed(input);
+
+            if(stateMachine.getCurrentState().equals("exit")){
+                break;
+            }
+        }
+
+        s.close();
     }
 }
---
+++ src/StateMachine.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class StateMachine {
+    
+    private String currentState = "main";
+
+    private Scanner s;
+
+    public StateMachine(Scanner s){
+        this.s = s;
+    }
+
+    private String[][] transitionTable = new String[][] 
+    {
+    //      Current      Input       Next       onTransition
+    //   +------------+----------+-----------+--------------------+
+        {   "main",       "0",     "exit",      "none"               },
+        {   "main",       "1",     "cities",    "citiesMenu"         },
+        {   "cities",     "0",     "main",      "mainMenu"           },
+        {   "cities",     "1",     "cities",    "insertCity"         },
+        {   "cities",     "2",     "cities",    "deleteCity"         },
+        {   "cities",     "3",     "cities",    "updateCityPop"      }
+    };
+
+    public String feed(String input){
+        for(int i = 0; i < transitionTable.length; i++){
+            if(transitionTable[i][0].equals(currentState) && transitionTable[i][1].equals(input)){
+
+                currentState = transitionTable[i][2];
+
+                onTransition(transitionTable[i][3]);
+
+                break;
+
+            }
+        }
+
+        return currentState;
+    }
+
+    public void onTransition(String action){
+        switch (action){
+            case "mainMenu":
+                DatabaseFrontend.mainMenu();
+                break;
+            case "citiesMenu":
+                DatabaseFrontend.citiesMenu();
+                break;
+            case "insertCity":
+                DatabaseFrontend.insertCityMenu(s);
+                DatabaseFrontend.citiesMenu();
+                break;
+            case "deleteCity":
+                DatabaseFrontend.deleteCityMenu(s);
+                DatabaseFrontend.citiesMenu();
+                break;
+            case "updateCityPop":
+                DatabaseFrontend.updateCityPopMenu(s);
+                DatabaseFrontend.citiesMenu();
+                break;
+            case "none":
+                break;
+        }
+    }
+
+    public String getCurrentState(){
+        return currentState;
+    }
+
+    public void setCurrentState(String state){
+        this.currentState = state;
+    }
+}
